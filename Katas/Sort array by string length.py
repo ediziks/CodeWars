35d617c20c07b9a3d7639501b6840802a4c268f6
Ediz Emektas <eemektas@gmail.com>
2019-09-17 18:23:30 +0300

    upload

--- Katas/Sort array by string length.py
+++ Katas/Sort array by string length.py
@@ -1,4 +1,5 @@
-'''Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
+'''
+Write a function that takes an array of strings as an argument and returns a sorted array containing the same strings, ordered from shortest to longest.
 
 For example, if this array were passed as an argument:
 
@@ -8,7 +9,8 @@ Your function would return the following array:
 
 ["Eyes", "Glasses", "Monocles", "Telescopes"]
 
-All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.'''
+All of the strings in the array passed to your function will be different lengths, so you will not have to decide how to order multiple strings of the same length.
+'''
 
 
 def sort_by_length(arr):
